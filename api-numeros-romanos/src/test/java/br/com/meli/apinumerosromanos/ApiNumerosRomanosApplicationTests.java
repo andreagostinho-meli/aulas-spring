d1613b071123146fc08b8c4a06b20bdd07a84c7b
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-06-30 11:47:02 -0300

    exercício em grupo: api números romanos

---
+++ api-numeros-romanos/src/test/java/br/com/meli/apinumerosromanos/ApiNumerosRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.apinumerosromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNumerosRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
